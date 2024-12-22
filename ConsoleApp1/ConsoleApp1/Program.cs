be17748405a1b0dc3ad8ca4e592b9099666a2efe
Alexsandr <manllac35@gmail.com>
2024-12-22 13:14:58 +0400

    HomeWork

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            if (!Int32.TryParse(Console.ReadLine(),out var a))
+            {
+                Console.WriteLine("Not a number!");
+                return;
+            }
+
+            if (!Int32.TryParse(Console.ReadLine(), out var b))
+            {
+                Console.WriteLine("Not a number!");
+                return;
+            }
+
+            var s = Console.ReadLine();
+            var boolVar = true;
+            if (s.Length == 0 || s.Length > 1 && !boolVar)
+            {
+                Console.WriteLine("Wrong sign");
+            }
+            switch(s[0])
+            {
+                case '+':
+                    Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("Result of {0} - {1} = {2}", a, b, a - b);
+                    break;
+                case '*':
+                    Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+                    break;
+                case '/':
+                    Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
+                    break;
+                case '%':
+                    Console.WriteLine("Result of {0} % {1} = {2}", a, b, a % b);
+                    break;
+                case '&':
+                    int Res1 = a & b;
+                    Console.WriteLine("Result of {0} & {1} = {2} (Decimal: {3}, Binary: {4}, Hexadecimal: {5})",
+                        a, b, Res1, Res1, Convert.ToString(Res1, 2), Res1.ToString("X"));
+                    break;
+                case '|':
+                    int Res2 = a | b;
+                    Console.WriteLine("Result of {0} | {1} = {2} (Decimal: {3}, Binary: {4}, Hexadecimal: {5})",
+                        a, b, Res2, Res2, Convert.ToString(Res2, 2), Res2.ToString("X"));
+                    break;
+                case '^':
+                    int Res3 = a ^ b;
+                    Console.WriteLine("Result of {0} ^ {1} = {2} (Decimal: {3}, Binary: {4}, Hexadecimal: {5})",
+                        a, b, Res3, Res3, Convert.ToString(Res3, 2), Res3.ToString("X"));
+                    break;
+                default:
+                    Console.WriteLine("Wrong sign");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
