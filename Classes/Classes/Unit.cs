87ba8c8ef7db0910e6fd56f50727c829f101c9ce
Alexsandr <manllac35@gmail.com>
2025-01-14 19:55:22 +0400

    Classes HomeWork

---
+++ Classes/Classes/Unit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Unit
+    {
+        private float _health = 100f;
+        private float _armor = 0.6f;
+        private int _damage = 5;
+        
+        public string Name { get; }
+
+        public float Health => _health;
+
+        public float Armor => _armor;
+
+        public Unit () : this (name: "Unknow Unit")
+        {
+
+        }
+
+
+        public Unit(string name)
+        {
+            Name = name;
+        }
+
+        public float GetRealHelth ()
+        {
+            return Health * (1f + Armor);
+        }
+
+        public bool SetDamage (float value)
+        {
+            _health -= value * Armor;
+            return _health < 0f;
+        }
+    }
+}
